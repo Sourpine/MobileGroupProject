aec21ba790122a6e932d98a4e3e3c7122cd5454a
Robert James <20.robert.james@ksd.org>
2019-01-31 14:00:10 -0800

    Added a drag script

---
+++ MobileGroupGame/Assets/GuiScripts/DragCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("EventData");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("EventData2");
+
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EventData1");
+    }
+
+
+}
