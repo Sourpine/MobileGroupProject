062911ac6f85fa478e56517a2a3ac8c439013483
Robert James <20.robert.james@ksd.org>
2019-02-04 13:58:20 -0800

    Continued Drag and Drop, started on button for card withdrawl

---
+++ MobileGroupGame/Assets/GuiScripts/ButtonForL2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonForL2 : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        d = eventData.OnPointerDown.GetComponent<ButtonForLookingAtCard>();
+        if (d.enabled == false)
+        {
+            d.enabled = true;
+        }
+    }
+}
