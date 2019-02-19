cea3512dd01996a414c8ead83f07baa84a5793ee
Robert James <Jamesrobbie207@gmail.com>
2019-02-19 06:49:12 -0800

    Finished AI, Finished Player Controller, Finished Board Controller, Waiting on Imaging to polish game

---
+++ MobileGroupGame/Assets/AiScripts/CardGameBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class CardGameBase : MonoBehaviour
+{
+    public string _name = "";
+    public override string ToString()
+    {
+        return _name;
+    }
+}
+
